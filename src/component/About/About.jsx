9a5db67387dda85ee575d2dc5583015cbd0ed2be
Lucky <82679510+LuckySingh9897@users.noreply.github.com>
2024-01-31 15:47:12 +0530

    Frontend about page done

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 
+import "./About.css"
+import { Typography } from '@mui/material'
+import profile from "../../images/profile.jpg"
+
 const About = () => {
   return (
-    <div>
-      About
+   <div className="about">
+    <div className="aboutContainer1">
+        <Typography>This is a Sample Quote</Typography>
     </div>
+    <div className="aboutContainer2">
+    <div>
+        <img src={profile} alt="profile-photo" className="aboutAvatar"/>
+        <Typography variant="h4" style={{margin:"1vmax 0", color:"black"}}>Lucky Singh</Typography>
+        <Typography>Full Stack Developer</Typography>
+        <Typography style={{margin:"1vmax 0"}}>I am Competitive Programmer</Typography>
+        <Typography>(Specialist) at Codeforces</Typography>
+
+    </div>
+    <div>
+        <Typography
+        style={{
+            wordSpacing:"5px",
+            lineHeight:"50px",
+            letterSpacing:"5px",
+            textAlign:"right"
+
+        }}
+        >This is Description Lorem ipsum dolor 
+            sit amet consectetur adipisicing elit. 
+            Minus nam rem eligendi, facere accusantium placeat
+             adipisci? Officiis quaerat eos delectus porro!</Typography>
+
+    </div>
+
+    </div>
+   </div>
   )
 }
 
